2516743443e0b006fa65c83ae1eea89f110ed98a
K.Anais <kiamanais@gmail.com>
2021-03-01 19:47:19 +0100

    form script

--- js/script.js
+++ js/script.js
@@ -16,4 +16,25 @@ $(document).ready(function() {
     $(".hidden3").toggle();
   });
 });
+$(document).ready(function(){
+  return function validate(){
+    let nameInput = $('#name')
+    let myForm = $('#myForm')
+    let name = $('#name').val();
+    let email = $('#email').val();
+    let message = $('#message').val();
 
+    if (name == '' || email =='' || message ==''){
+      alert('Fill all fields')
+      nameInput.addClass('error')
+      return false;
+    }
+    else{
+      let link = 'https://gmail.us1.list-manage.com/subscribe/post?u=52726355d5081b4ef5b84cb09&amp;id=6b786bf150'
+      myForm.attr('action', link)
+      myForm.attr('method', 'post')
+      let response = `Hey ${name}, your message has been received`
+      alert(response);
+    }
+  }
+});
