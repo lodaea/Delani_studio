f7c1e2aca2b0a53f5c22091386fb6b3701c5b78c
K.Anais <kiamanais@gmail.com>
2021-03-01 12:11:43 +0100

    pushing script

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $(".hdiv1").click(function() {
+    $(".show1").toggle();
+    $(".hidden1").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".hdiv2").click(function() {
+    $(".show2").toggle();
+    $(".hidden2").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".hdiv3").click(function() {
+    $(".show3").toggle();
+    $(".hidden3").toggle();
+  });
+});
